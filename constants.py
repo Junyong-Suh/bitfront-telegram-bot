d114679d4421a973209cf3df495c720fb1de0887
Yoshi <junyongsuh@gmail.com>
2020-03-22 14:43:50 +0900

    chore: constants.py

---
+++ constants.py
@@ -0,0 +1,14 @@
+# bitfront response keys
+KEY_RESPONSE_DATA = 'responseData'
+KEY_LAST = 'last'
+
+ETH_USD = 'eth_usd'
+BTC_USD = 'btc_usd'
+LN_BTC = 'ln_btc'
+LN_USD = 'ln_usd'
+TIMESTAMP_UTC = 'timestamp_utc'
+DATETIME_UTC = 'datetime_utc'
+
+ONE_MIN_IN_SEC = 60
+TEN_MIN_IN_SEC = ONE_MIN_IN_SEC*10
+ONE_HOUR_IN_SEC = TEN_MIN_IN_SEC*6
