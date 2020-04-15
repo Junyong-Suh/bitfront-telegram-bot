b1af333e0181b0781aa806cb443ec378e46ff644
Yoshi <junyongsuh@gmail.com>
2020-04-15 16:12:31 +0900

    chore: script updates

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,30 +8,30 @@ NC='\033[0m' # No Color
 LAST_TAG=$(./get_last_tag.sh) # execute and capture stdout
 STATUS=$? # find out exit status
 if [ $STATUS -eq 0 ]; then
-  printf "${CYAN}Current Git and Docker tag: %s${NC}" "$LAST_TAG"
+  printf "${CYAN}Current Git and Docker tag: ${LAST_TAG}${NC}\n"
 else
-  printf "%s" "$LAST_TAG"
+  printf "${LAST_TAG}"
   exit 1
 fi
 
 # start deploying
 DOCKER_HUB="zechery/bitfront-price-alert"
 NEW_IMAGE="$DOCKER_HUB:$LAST_TAG"
-printf "${CYAN}Deploy %s${NC}" "$NEW_IMAGE"
+printf "${CYAN}Deploy ${NEW_IMAGE}${NC}\n"
 
 # stop and remove current containers
-printf "${CYAN}Stop all running containers:${NC}"
+printf "${CYAN}Stop all running containers:${NC}\n"
 docker stop "$(docker ps -aq)"
-printf "${CYAN}Remove all containers:${NC}"
+printf "${CYAN}Remove all containers:${NC}\n"
 docker rm "$(docker ps -aq)"
 
 # pull and run the new image
-printf "${CYAN}Run $NEW_IMAGE:${NC}"
+printf "${CYAN}Run ${NEW_IMAGE}:${NC}\n"
 docker run -d "$NEW_IMAGE"
 
 # done deploying, display images and containers
-printf "${CYAN}Docker images:${NC}"
+printf "${CYAN}Docker images:${NC}\n"
 docker images
-printf "${CYAN}Docker containers:${NC}"
+printf "${CYAN}Docker containers:${NC}\n"
 docker ps
-printf "${GREEN}Deployed %s${NC}" "$NEW_IMAGE"
+printf "${GREEN}Deployed ${NEW_IMAGE}${NC}\n"
