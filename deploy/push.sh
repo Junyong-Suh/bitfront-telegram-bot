a3c74e656f930acb41a030ed211e04cd3b2689a5
Yoshi <junyongsuh@gmail.com>
2020-04-15 16:07:54 +0900

    chore: script updates

--- deploy/push.sh
+++ deploy/push.sh
@@ -12,19 +12,19 @@ DOCKER_HUB="zechery/bitfront-price-alert"
 NEW_IMAGE="$DOCKER_HUB:$NEW_TAG"
 
 # commit
-printf "${CYAN}Commit and push to Git '$CC_TYPE: $COMMIT_MSG'${NC}\n"
+printf "${CYAN}Commit and push to Git ${CC_TYPE}: ${COMMIT_MSG} ${NC}\n"
 git commit -am "$CC_TYPE: $COMMIT_MSG"
 git push origin master
 
 # tag
-printf "${CYAN}Add and push a tag $NEW_TAG: $COMMIT_MSG${NC}\n"
+printf "${CYAN}Add and push a tag ${NEW_TAG}: ${COMMIT_MSG}${NC}\n"
 git tag -a "$NEW_TAG" -m "$NEW_TAG: $COMMIT_MSG"
 git push origin "$NEW_TAG"
 
 # docker build & push
-printf "${CYAN}Build new Docker image $NEW_IMAGE${NC}\n"
+printf "${CYAN}Build new Docker image ${NEW_IMAGE}${NC}\n"
 docker build -t "$NEW_IMAGE" .
-printf "${CYAN}Push new Docker image $NEW_IMAGE${NC}\n"
+printf "${CYAN}Push new Docker image ${NEW_IMAGE}${NC}\n"
 docker push "$NEW_IMAGE"
 
 printf "${GREEN}Completed successfully${NC}\n"
